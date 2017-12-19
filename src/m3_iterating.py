0a238965392dee433ac7e91f5c1af5a5f4924114
keyjw <keyjw@rose-hulman.edu>
2017-12-18 21:11:17 -0500

    m3 in progress

--- src/m3_iterating.py
+++ src/m3_iterating.py
@@ -4,23 +4,23 @@ in its most classic form:
   -- Iterate all the way through the sequence, from beginning to end.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Joshua Key.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_count_negatives()
-    run_test_count_short_ones()
+    # run_test_count_negatives()
+    # run_test_count_short_ones()
     run_test_draw_circles()
 
 
 def run_test_count_negatives():
     """ Tests the   count_negatives   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  count_negatives  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests beyond those we wrote.
     #
@@ -83,6 +83,20 @@ def run_test_count_negatives():
 
     # TO DO 2 (continued):  Add your 2 ADDITIONAL tests here:
 
+    # Test 6:
+    expected = 2
+    actual = count_negatives((-8, 4, 100, -6))
+    print()
+    print('Test 6 expected:', expected)
+    print('       actual:  ', actual)
+
+    # Test 7:
+    expected = 5
+    actual = count_negatives((-1, 69, -7, 20, -3, -84, -2))
+    print()
+    print('Test 7 expected:', expected)
+    print('       actual:  ', actual)
+
 
 def count_negatives(seq):
     """
@@ -99,8 +113,13 @@ def count_negatives(seq):
     Type hints:
       :type seq: list or tuple
     """
+    count = 0
+    for k in range(len(seq)):
+        if seq[k] < 0:
+            count = count + 1
+    return count
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
 
@@ -285,6 +304,12 @@ def draw_circles(window, points, radius, color):
       :type radius: float
       :type color: str
     """
+    for k in range(len(points)):
+        center = rg.Point(points[0], points[1])
+        circle = rg.Circle(center, radius)
+        circle.fill_color = color
+        circle.attach_to(window)
+    window.render()
     # ------------------------------------------------------------------
     # TODO: 6. Implement and test this function.
     #          Tests have been written for you (above).
